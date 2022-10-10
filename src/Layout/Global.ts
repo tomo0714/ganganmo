c8b20448013fc40576a84c4501bc4bc406048f12
tomoyoshi <tomo.nyaaaaan.714@gmail.com>
2022-10-10 16:27:39 +0900

    💄 global cssを適応

---
+++ src/Layout/Global.ts
@@ -0,0 +1,16 @@
+import { css } from '@emotion/react'
+
+export const global = css`
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  li {
+    list-style-type: none;
+  }
+
+  ul {
+    padding: 0;
+  }
+`
