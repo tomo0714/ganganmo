43963a50f8f115b465fabfda5460792031bbbef2
tomoyoshi <tomo.nyaaaaan.714@gmail.com>
2022-11-08 22:17:33 +0900

    ➕ Recoilを導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,15 @@
 import type { AppProps } from 'next/app'
 import 'modern-css-reset/dist/reset.min.css'
+import { RecoilRoot } from 'recoil'
 import { Layout } from '@/components/Layout'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <RecoilRoot>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </RecoilRoot>
   )
 }
 
