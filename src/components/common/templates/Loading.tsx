072ca2c3f972440050241aed3c4353da86dbe90e
tomoyoshi <tomo.nyaaaaan.714@gmail.com>
2023-04-20 22:51:29 +0900

    💄 ローディング画面をつばさマンに変更

--- src/components/common/templates/Loading.tsx
+++ src/components/common/templates/Loading.tsx
@@ -7,42 +7,31 @@ const LoadingStyle = css`
   position: absolute;
   top: 50%;
   left: 50%;
-  margin-top: -10px;
-  margin-left: -50px;
-  background-color: #fff;
   text-align: center;
+
+  img {
+    width: 300px;
+  }
+
+  .rotate-image {
+    animation: rotate-animation 2s linear infinite;
+  }
+
+  @keyframes rotate-animation {
+    0% {
+      transform: rotate(0deg);
+    }
+
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 
   p {
     margin-top: 5px;
     font-family: 'Courier Prime', monospace;
     font-size: 20px;
     font-weight: bold;
-  }
-
-  .bar {
-    display: block;
-    width: 100px;
-    height: 5px;
-    margin-top: 5px;
-    animation: load 2s infinite;
-    background: #000;
-    border-radius: 5px;
-  }
-
-  .middle {
-    animation-delay: 100ms;
-  }
-
-  .bottom {
-    animation-delay: 200ms;
-  }
-
-  @keyframes load {
-    50% {
-      width: 5px;
-      margin-right: 95px;
-      opacity: 0.1;
-    }
   }
 `
 
@@ -58,10 +47,8 @@ export const Loading = (props: LoadingProps) => {
     <>
       {isLoading ? (
         <div css={LoadingStyle}>
-          <span className="bar top"></span>
-          <span className="bar middle"></span>
-          <span className="bar bottom"></span>
-          <p>LOADING</p>
+          <p>LOADING...</p>
+          <img className="rotate-image" src="/tubasaMan.png" />
         </div>
       ) : (
         children
